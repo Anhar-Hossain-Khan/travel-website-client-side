93afae5c8ae9814c3a2f71e6fd1614a9a4358baa
Anhar-Hossain-Khan <anhar.hossain1997@gmail.com>
2021-10-31 09:10:38 +0600

    deleted created

--- src/components/Home/MyOrders/MyOrders.js
+++ src/components/Home/MyOrders/MyOrders.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Row  } from 'react-bootstrap';
-import MyOrder from '../MyOrder/MyOrder';
-
+import { Row, Table  } from 'react-bootstrap';
 
 const MyOrders = () => {
     const [myOrders, setmyOrders] = useState([])
@@ -12,15 +10,63 @@ const MyOrders = () => {
         .then(data => setmyOrders(data))
     }, [])
 
+     // Delete an user
+
+     const handleDeleteUser = id =>{
+        const proceed = window.confirm('Are you sure, you want to Cancel?');
+        if(proceed){
+            const url = `https://desolate-spire-46689.herokuapp.com/orderplace/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.deletedCount > 0){
+                alert('Cancel Successfully');
+                const remainingMyOrders = myOrders.filter(myOrder => myOrder._id !== id)
+                setmyOrders(remainingMyOrders);
+            }
+        })
+        } 
+    }
+
     return (
         <div className="container">
         <div className="top">
         <h2>My <span>Orders</span></h2>
         </div>
         <Row xs={1} md={1} lg={1} className="g-4">
-            {
-                myOrders?.map(myOrder => <MyOrder key={myOrder._id} myOrder={myOrder}></MyOrder>)
+           <ol>
+           {
+                myOrders?.map(myOrder => <li key={myOrder._id}>
+  <div className="mt-5">
+  <Table striped bordered hover className="text-primary" responsive="sm md lg">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Address</th>
+      <th>City</th>
+      <th>Phone</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{myOrder.name}</td>
+      <td>{myOrder.email}</td>
+      <td>{myOrder.address}</td>
+      <td>{myOrder.city}</td>
+      <td>{myOrder.phone}</td>
+      <td> <button onClick={()=> handleDeleteUser(myOrder._id)}  className=" text-white btn btn-success">Cancel</button> </td>
+    </tr>
+    
+  </tbody>
+  </Table>
+        </div>
+                </li>)
             }
+           </ol>
         </Row>
      </div>
 
